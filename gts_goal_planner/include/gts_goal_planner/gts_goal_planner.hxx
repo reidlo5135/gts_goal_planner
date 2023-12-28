8e3a7929f5e033262e74d7d1ace70f04b01f443f
reidlo5135 <201840103@email.daelim.ac.kr>
2023-12-28 15:37:33 +0900

    feat : add gts_goal_planner_server's exec cb

--- gts_goal_planner/include/gts_goal_planner/gts_goal_planner.hxx
+++ gts_goal_planner/include/gts_goal_planner/gts_goal_planner.hxx
@@ -10,6 +10,8 @@
 #include <rclcpp_action/rclcpp_action.hpp>
 #include <rclcpp/executors/multi_threaded_executor.hpp>
 #include <rcutils/logging_macros.h>
+#include <std_msgs/msg/string.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
 #include <action_msgs/msg/goal_status.hpp>
 #include <gts_msgs/action/goal_planner.hpp>
@@ -20,7 +22,11 @@
 #define RCL_NODE_NAME "gts_goal_planner"
 #define CLASS_NAME "gts_goal_planner"
 
-#define ACTION_SERVER_NAME "gts_goal_planner/planner"
+#define GTS_GOAL_PLANNER_SERVER_NAME "/gts_goal_planner/planner"
+#define NTP_CLIENT_NAME "/navigate_to_pose"
+
+#define DEFAULT_INT 0
+#define DEFAULT_DOUBLE 0.0
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -32,17 +38,28 @@ namespace gts_goal_planner
         class GoalPlanner : public rclcpp::Node
         {
         private:
+            int slam_waypoints_list_index_;
+            std::vector<sensor_msgs::msg::NavSatFix> slam_waypoints_list_;
+            size_t slam_waypoints_list_size_;
+
             rclcpp::Node::SharedPtr node_;
             rclcpp::CallbackGroup::SharedPtr gts_goal_planner_server_cb_group_;
             rclcpp_action::Server<gts_msgs::action::GoalPlanner>::SharedPtr gts_goal_planner_server_;
 
+            rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr ntp_goal_handle_;
+            rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::Goal::SharedPtr ntp_goal_;
             rclcpp::CallbackGroup::SharedPtr ntp_client_cb_group_;
             rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr ntp_client_;
 
-            rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const gts_msgs::action::GoalPlanner::Goal> goal);
-            rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle);
-            void handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle);
-            void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle);
+            std_msgs::msg::Header build_header(const char *frame_id);
+            rclcpp_action::GoalResponse gts_handle_goal(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const gts_msgs::action::GoalPlanner::Goal> goal);
+            rclcpp_action::CancelResponse gts_handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle);
+            void gts_handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle);
+            void gts_execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle);
+            void ntp_send_goal();
+            void ntp_feedback_cb(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr goal_handle, const std::shared_ptr<const nav2_msgs::action::NavigateToPose::Feedback> feedback);
+            void ntp_goal_response_cb(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr &goal_handle);
+            void ntp_result_cb(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult &wrapped_result);
             void position_test();
         public:
             explicit GoalPlanner(const rclcpp::NodeOptions &node_options);
--- gts_goal_planner/src/gts_goal_planner.cxx
+++ gts_goal_planner/src/gts_goal_planner.cxx
@@ -15,40 +15,185 @@ gts_goal_planner::planner::GoalPlanner::GoalPlanner(const rclcpp::NodeOptions &n
         exit(0);
     }
 
+    const rcl_action_server_options_t &gts_goal_planner_server_opts = rcl_action_server_get_default_options();
     this->gts_goal_planner_server_cb_group_ = this->node_->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
     this->gts_goal_planner_server_ = rclcpp_action::create_server<gts_msgs::action::GoalPlanner>(
         this->node_,
-        ACTION_SERVER_NAME,
-        std::bind(&gts_goal_planner::planner::GoalPlanner::handle_goal, this, _1, _2),
-        std::bind(&gts_goal_planner::planner::GoalPlanner::handle_cancel, this, _1),
-        std::bind(&gts_goal_planner::planner::GoalPlanner::handle_accepted, this, _1),
-        rcl_action_server_get_default_options(),
+        GTS_GOAL_PLANNER_SERVER_NAME,
+        std::bind(&gts_goal_planner::planner::GoalPlanner::gts_handle_goal, this, _1, _2),
+        std::bind(&gts_goal_planner::planner::GoalPlanner::gts_handle_cancel, this, _1),
+        std::bind(&gts_goal_planner::planner::GoalPlanner::gts_handle_accepted, this, _1),
+        gts_goal_planner_server_opts,
         this->gts_goal_planner_server_cb_group_);
+
+    this->ntp_goal_ = std::make_shared<rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::Goal>();
+    
+    const rcl_action_client_options_t &ntp_client_opts = rcl_action_client_get_default_options();
+    this->ntp_client_cb_group_ = this->node_->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+    this->ntp_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
+        this->node_,
+        NTP_CLIENT_NAME,
+        this->ntp_client_cb_group_,
+        ntp_client_opts);
 }
 
 gts_goal_planner::planner::GoalPlanner::~GoalPlanner()
 {
-
 }
 
-rclcpp_action::GoalResponse gts_goal_planner::planner::GoalPlanner::handle_goal(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const gts_msgs::action::GoalPlanner::Goal> goal)
+rclcpp_action::GoalResponse gts_goal_planner::planner::GoalPlanner::gts_handle_goal(const rclcpp_action::GoalUUID &uuid, std::shared_ptr<const gts_msgs::action::GoalPlanner::Goal> goal)
+{
+    (void)uuid;
+
+    RCLCPP_INFO(this->node_->get_logger(), "gts handle_goal");
+
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse gts_goal_planner::planner::GoalPlanner::gts_handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle)
+{
+    RCLCPP_INFO(this->node_->get_logger(), "gts cancel_goal");
+
+    (void)goal_handle;
+
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void gts_goal_planner::planner::GoalPlanner::gts_handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle)
+{
+    std::thread{std::bind(&gts_goal_planner::planner::GoalPlanner::gts_execute, this, _1), goal_handle}.detach();
+}
+
+void gts_goal_planner::planner::GoalPlanner::gts_execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle)
+{
+    RCLCPP_INFO(this->node_->get_logger(), "gts execute_goal");
+    const std::shared_ptr<const gts_msgs::action::GoalPlanner_Goal> goal = goal_handle->get_goal();
+
+    std::vector<sensor_msgs::msg::NavSatFix, std::allocator<sensor_msgs::msg::NavSatFix>> goal_request = goal->gps_goal_list;
+    this->slam_waypoints_list_ = goal_request;
+    this->slam_waypoints_list_index_ = 0;
+    this->slam_waypoints_list_size_ = this->slam_waypoints_list_.size();
+
+    RCLCPP_INFO(this->node_->get_logger(), "gts execute_goal list size : [%d]", slam_waypoints_list_size_);
+
+    this->ntp_send_goal();
+
+    std::shared_ptr<gts_msgs::action::GoalPlanner::Feedback> feedback = std::make_shared<gts_msgs::action::GoalPlanner::Feedback>();
+    std::shared_ptr<gts_msgs::action::GoalPlanner::Result> result = std::make_shared<gts_msgs::action::GoalPlanner::Result>();
+
+    goal_handle->succeed(result);
+}
+
+void gts_goal_planner::planner::GoalPlanner::ntp_send_goal()
+{
+    bool is_slam_waypoints_list_empty = this->slam_waypoints_list_.empty();
+
+    if (is_slam_waypoints_list_empty)
+    {
+        RCLCPP_ERROR(this->node_->get_logger(), "ntp send goal slam_waypoints_list is empty...");
+        return;
+    }
+
+    const double &slam_x = this->slam_waypoints_list_[slam_waypoints_list_index_].longitude;
+    const double &slam_y = this->slam_waypoints_list_[slam_waypoints_list_index_].latitude;
+
+    bool is_navigate_to_pose_server_ready = this->ntp_client_->wait_for_action_server(std::chrono::seconds(5));
+
+    if (!is_navigate_to_pose_server_ready)
+    {
+        RCLCPP_ERROR(this->node_->get_logger(), "ntp action server is not available after waiting... vacate SLAM waypoints_list");
+        this->slam_waypoints_list_.clear();
+        this->slam_waypoints_list_index_ = DEFAULT_INT;
+        this->slam_waypoints_list_size_ = DEFAULT_INT;
+        return;
+    }
+
+    RCLCPP_INFO(
+        this->node_->get_logger(),
+        "ntp action server is ready\n\tslam x : [%f]\n\tslam y : [%f]",
+        slam_x,
+        slam_y);
+
+    geometry_msgs::msg::PoseStamped::UniquePtr geometry_msgs_pose_stamped = std::make_unique<geometry_msgs::msg::PoseStamped>();
+
+    geometry_msgs::msg::Point::UniquePtr geometry_msgs_point = std::make_unique<geometry_msgs::msg::Point>();
+    geometry_msgs_point->set__x(slam_x);
+    geometry_msgs_point->set__y(slam_y);
+    geometry_msgs_point->set__z(DEFAULT_DOUBLE);
+
+    geometry_msgs::msg::Quaternion::UniquePtr geometry_msgs_quaternion = std::make_unique<geometry_msgs::msg::Quaternion>();
+    geometry_msgs_quaternion->set__x(DEFAULT_DOUBLE);
+    geometry_msgs_quaternion->set__y(DEFAULT_DOUBLE);
+    geometry_msgs_quaternion->set__z(DEFAULT_DOUBLE);
+    geometry_msgs_quaternion->set__w(DEFAULT_DOUBLE);
+
+    geometry_msgs::msg::Pose::UniquePtr geometry_msgs_pose = std::make_unique<geometry_msgs::msg::Pose>();
+
+    const geometry_msgs::msg::Point &&geometry_msgs_point_moved = std::move(*geometry_msgs_point);
+    geometry_msgs_pose->set__position(geometry_msgs_point_moved);
+
+    const geometry_msgs::msg::Quaternion &&geometry_msgs_quaternion_moved = std::move(*geometry_msgs_quaternion);
+    geometry_msgs_pose->set__orientation(geometry_msgs_quaternion_moved);
+
+    const geometry_msgs::msg::Pose &&geometry_msgs_pose_moved = std::move(*geometry_msgs_pose);
+    geometry_msgs_pose_stamped->set__pose(geometry_msgs_pose_moved);
+
+    const geometry_msgs::msg::PoseStamped &&geometry_msgs_pose_stamped_moved = std::move(*geometry_msgs_pose_stamped);
+    this->ntp_goal_->set__pose(geometry_msgs_pose_stamped_moved);
+
+    rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions navigate_to_pose_send_goal_opts = rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
+    navigate_to_pose_send_goal_opts.feedback_callback = std::bind(&gts_goal_planner::planner::GoalPlanner::ntp_feedback_cb, this, _1, _2);
+    navigate_to_pose_send_goal_opts.goal_response_callback = std::bind(&gts_goal_planner::planner::GoalPlanner::ntp_goal_response_cb, this, _1);
+    navigate_to_pose_send_goal_opts.result_callback = std::bind(&gts_goal_planner::planner::GoalPlanner::ntp_result_cb, this, _1);
+
+    std::shared_future<rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr> navigate_to_pose_goal_future = this->ntp_client_->async_send_goal(*ntp_goal_, navigate_to_pose_send_goal_opts);
+    this->ntp_goal_handle_ = navigate_to_pose_goal_future.get();
+
+    RCLCPP_INFO(
+        this->node_->get_logger(),
+        "ntp goal sent\n\tpose_x : [%f]\n\tpose_y : [%f]\n\torien_z: [%f]\n\torien_w : [%f]",
+        ntp_goal_->pose.pose.position.x,
+        ntp_goal_->pose.pose.position.y,
+        ntp_goal_->pose.pose.orientation.z,
+        ntp_goal_->pose.pose.orientation.w);
+}
+
+void gts_goal_planner::planner::GoalPlanner::ntp_feedback_cb(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr goal_handle, const std::shared_ptr<const nav2_msgs::action::NavigateToPose::Feedback> feedback)
 {
 
 }
 
-rclcpp_action::CancelResponse gts_goal_planner::planner::GoalPlanner::handle_cancel(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle)
+void gts_goal_planner::planner::GoalPlanner::ntp_goal_response_cb(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr &goal_handle)
 {
 
 }
 
-void gts_goal_planner::planner::GoalPlanner::handle_accepted(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle)
+void gts_goal_planner::planner::GoalPlanner::ntp_result_cb(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult &wrapped_result)
 {
 
 }
 
-void gts_goal_planner::planner::GoalPlanner::execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<gts_msgs::action::GoalPlanner>> goal_handle)
+std_msgs::msg::Header gts_goal_planner::planner::GoalPlanner::build_header(const char *frame_id)
 {
+    std::chrono::_V2::system_clock::time_point current_time = std::chrono::system_clock::now();
+    std::chrono::_V2::system_clock::duration current_time_duration = current_time.time_since_epoch();
 
+    const int32_t &current_time_sec = std::chrono::duration_cast<std::chrono::seconds>(current_time_duration).count();
+    const int32_t &current_time_nanosec = current_time_sec % 1000000000;
+
+    builtin_interfaces::msg::Time::UniquePtr time = std::make_unique<builtin_interfaces::msg::Time>();
+    time->set__sec(current_time_sec);
+    time->set__nanosec(current_time_nanosec);
+
+    std_msgs::msg::Header::UniquePtr header = std::make_unique<std_msgs::msg::Header>();
+    header->set__frame_id(frame_id);
+
+    const builtin_interfaces::msg::Time &&time_moved = std::move(*time);
+    header->set__stamp(time_moved);
+
+    const std_msgs::msg::Header &&header_moved = std::move(*header);
+
+    return header_moved;
 }
 
 void gts_goal_planner::planner::GoalPlanner::position_test()
